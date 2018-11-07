7d0b2637a3463b87f948aa1b1584f39039d0edb7
Alexander Lokhman <alex.lokhman@gmail.com>
2018-11-07 10:25:32 +0000

    Combine length constraints in one go-file

--- validator/length.go
+++ validator/length.go
@@ -9,6 +9,7 @@ import (
 
 type Length uint
 
+// Length
 func (c Length) Validate(value interface{}, _ types.Flag) ErrorInterface {
 	v := helpers.Indirect(reflect.ValueOf(value))
 	switch v.Kind() {
@@ -32,4 +33,60 @@ func (_ Length) Strict() bool {
 
 func (_ Length) Name() string {
 	return "length"
+}
+
+// MinLength
+type MinLength uint
+
+func (c MinLength) Validate(value interface{}, _ types.Flag) ErrorInterface {
+	v := helpers.Indirect(reflect.ValueOf(value))
+	switch v.Kind() {
+	case reflect.String:
+		if v.Len() < int(c) {
+			return NewConstraintError(c, "this value should have %d character(s) or more", c)
+		}
+	case reflect.Array, reflect.Slice, reflect.Map, reflect.Chan:
+		if v.Len() < int(c) {
+			return NewConstraintError(c, "this value should contain %d element(s) or more", c)
+		}
+	default:
+		return UnexpectedTypeError(c, value)
+	}
+	return nil
+}
+
+func (_ MinLength) Strict() bool {
+	return false
+}
+
+func (_ MinLength) Name() string {
+	return "min_length"
+}
+
+// MaxLength
+type MaxLength uint
+
+func (c MaxLength) Validate(value interface{}, _ types.Flag) ErrorInterface {
+	v := helpers.Indirect(reflect.ValueOf(value))
+	switch v.Kind() {
+	case reflect.String:
+		if v.Len() > int(c) {
+			return NewConstraintError(c, "this value should have %d character(s) or less", c)
+		}
+	case reflect.Array, reflect.Slice, reflect.Map, reflect.Chan:
+		if v.Len() > int(c) {
+			return NewConstraintError(c, "this value should contain %d element(s) or less", c)
+		}
+	default:
+		return UnexpectedTypeError(c, value)
+	}
+	return nil
+}
+
+func (_ MaxLength) Strict() bool {
+	return false
+}
+
+func (_ MaxLength) Name() string {
+	return "max_length"
 }
