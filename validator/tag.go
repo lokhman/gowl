e41e9c92b46e0d1407c474b19653981937045fc3
Alexander Lokhman <alex.lokhman@gmail.com>
2018-11-08 11:37:14 +0000

    Rewrite tag parser

--- validator/tag.go
+++ validator/tag.go
@@ -2,27 +2,31 @@ package validator
 
 import (
 	"fmt"
-	"regexp"
 	"strconv"
+	"strings"
+
+	"github.com/lokhman/gowl/helpers"
 )
 
-var reTag = regexp.MustCompile(`([^,=]+)(?:=(\[.*?]|".*?"|[^,]+))?`)
-
 type TagOption struct {
-	Name  string
-	Value string
+	Name   string
+	Value  string
+	Prefix string
 }
 
 func (o TagOption) NoValue(lambda func() ConstraintInterface) (ConstraintInterface, error) {
-	if o.Value != "" {
+	if o.Value != "" || o.Prefix != "" {
 		return nil, fmt.Errorf(`gowl/validator: tag option "%s" should not have value`, o.Name)
 	}
 	return lambda(), nil
 }
 
-func (o TagOption) WithBool(lambda func(v bool) ConstraintInterface, defaultValue bool) (ConstraintInterface, error) {
+func (o TagOption) WithBool(lambda func(v bool) ConstraintInterface) (ConstraintInterface, error) {
+	if o.Prefix != "" {
+		return nil, fmt.Errorf(`gowl/validator: tag option "%s" has invalid bool prefix "%s"`, o.Name, o.Prefix)
+	}
 	if o.Value == "" {
-		return lambda(defaultValue), nil
+		return lambda(true), nil
 	}
 	v, err := strconv.ParseBool(o.Value)
 	if err != nil {
@@ -31,24 +35,61 @@ func (o TagOption) WithBool(lambda func(v bool) ConstraintInterface, defaultValu
 	return lambda(v), nil
 }
 
-func (o TagOption) WithInt(lambda func(v int) ConstraintInterface) (ConstraintInterface, error) {
-	v, err := strconv.Atoi(o.Value)
+func (o TagOption) WithInt(lambda func(v int64) ConstraintInterface) (ConstraintInterface, error) {
+	var bitSize int
+	switch o.Prefix {
+	case "i8":
+		bitSize = 8
+	case "i16":
+		bitSize = 16
+	case "i32":
+		bitSize = 32
+	case "i64":
+		bitSize = 64
+	case "i", "": // 0
+	default:
+		return nil, fmt.Errorf(`gowl/validator: tag option "%s" has invalid int prefix "%s"`, o.Name, o.Prefix)
+	}
+	v, err := strconv.ParseInt(o.Value, 10, bitSize)
 	if err != nil {
 		return nil, fmt.Errorf(`gowl/validator: tag option "%s" has invalid int value: %s`, o.Name, err)
 	}
 	return lambda(v), nil
 }
 
-func (o TagOption) WithUint(lambda func(v uint) ConstraintInterface) (ConstraintInterface, error) {
-	v, err := strconv.ParseUint(o.Value, 10, 64)
+func (o TagOption) WithUint(lambda func(v uint64) ConstraintInterface) (ConstraintInterface, error) {
+	var bitSize int
+	switch o.Prefix {
+	case "u8":
+		bitSize = 8
+	case "u16":
+		bitSize = 16
+	case "u32":
+		bitSize = 32
+	case "u64":
+		bitSize = 64
+	case "u", "": // 0
+	default:
+		return nil, fmt.Errorf(`gowl/validator: tag option "%s" has invalid uint prefix "%s"`, o.Name, o.Prefix)
+	}
+	v, err := strconv.ParseUint(o.Value, 10, bitSize)
 	if err != nil {
 		return nil, fmt.Errorf(`gowl/validator: tag option "%s" has invalid uint value: %s`, o.Name, err)
 	}
-	return lambda(uint(v)), nil
+	return lambda(v), nil
 }
 
 func (o TagOption) WithFloat(lambda func(v float64) ConstraintInterface) (ConstraintInterface, error) {
-	v, err := strconv.ParseFloat(o.Value, 64)
+	var bitSize int
+	switch o.Prefix {
+	case "f32":
+		bitSize = 32
+	case "f64", "f", "":
+		bitSize = 64
+	default:
+		return nil, fmt.Errorf(`gowl/validator: tag option "%s" has invalid float prefix "%s"`, o.Name, o.Prefix)
+	}
+	v, err := strconv.ParseFloat(o.Value, bitSize)
 	if err != nil {
 		return nil, fmt.Errorf(`gowl/validator: tag option "%s" has invalid float value: %s`, o.Name, err)
 	}
@@ -56,11 +97,51 @@ func (o TagOption) WithFloat(lambda func(v float64) ConstraintInterface) (Constr
 }
 
 func (o TagOption) WithString(lambda func(v string) ConstraintInterface) (ConstraintInterface, error) {
-	n := len(o.Value)
-	if n < 2 || o.Value[0] != '"' || o.Value[n-1] != '"' {
-		return nil, fmt.Errorf(`gowl/validator: tag option "%s" should be in format "..."`, o.Name)
+	if o.Prefix != "" {
+		return nil, fmt.Errorf(`gowl/validator: tag option "%s" has invalid string prefix "%s"`, o.Name, o.Prefix)
 	}
-	return lambda(o.Value[1 : n-1]), nil
+	n := len(o.Value)
+	if n < 2 || o.Value[0] != '\'' || o.Value[n-1] != '\'' {
+		return nil, fmt.Errorf(`gowl/validator: tag option "%s" should be in format '...'`, o.Name)
+	}
+	return lambda(helpers.StripSlashes(o.Value[1:n-1], `'"`)), nil
+}
+
+func (o TagOption) WithValue(lambda func(v interface{}) ConstraintInterface) (ConstraintInterface, error) {
+	if o.Value == "" {
+		return nil, fmt.Errorf(`gowl/validator: tag option "%s" should have value`, o.Name)
+	}
+	if o.Value[0] == '\'' {
+		return o.WithString(func(v string) ConstraintInterface { return lambda(v) })
+	}
+	switch o.Prefix {
+	case "i", "":
+		return o.WithInt(func(v int64) ConstraintInterface { return lambda(int(v)) })
+	case "i8":
+		return o.WithInt(func(v int64) ConstraintInterface { return lambda(int8(v)) })
+	case "i16":
+		return o.WithInt(func(v int64) ConstraintInterface { return lambda(int16(v)) })
+	case "i32":
+		return o.WithInt(func(v int64) ConstraintInterface { return lambda(int32(v)) })
+	case "i64":
+		return o.WithInt(func(v int64) ConstraintInterface { return lambda(int64(v)) })
+	case "u":
+		return o.WithUint(func(v uint64) ConstraintInterface { return lambda(uint(v)) })
+	case "u8":
+		return o.WithUint(func(v uint64) ConstraintInterface { return lambda(uint8(v)) })
+	case "u16":
+		return o.WithUint(func(v uint64) ConstraintInterface { return lambda(uint16(v)) })
+	case "u32":
+		return o.WithUint(func(v uint64) ConstraintInterface { return lambda(uint32(v)) })
+	case "u64":
+		return o.WithUint(func(v uint64) ConstraintInterface { return lambda(uint(v)) })
+	case "f32":
+		return o.WithFloat(func(v float64) ConstraintInterface { return lambda(float32(v)) })
+	case "f64", "f":
+		return o.WithFloat(func(v float64) ConstraintInterface { return lambda(float64(v)) })
+	default:
+		return nil, fmt.Errorf(`gowl/validator: tag option "%s" has invalid prefix "%s"`, o.Name, o.Prefix)
+	}
 }
 
 func (o TagOption) WithTag(lambda func(v []ConstraintInterface) ConstraintInterface) (ConstraintInterface, error) {
@@ -76,35 +157,89 @@ func (o TagOption) WithTag(lambda func(v []ConstraintInterface) ConstraintInterf
 }
 
 func ParseTag(tag string) (constraints []ConstraintInterface, err error) {
-	for _, match := range reTag.FindAllStringSubmatch(tag, -1) {
-		ci, ok := TagOptions[match[1]]
-		if !ok {
-			return nil, fmt.Errorf(`gowl/validator: unknown tag option "%s"`, match[1])
+	quotes, brackets := 0, 0
+	tokenStart := 0
+
+	tag += ","
+	for i, c := range tag {
+		switch {
+		case c == '[' && quotes%2 == 0:
+			brackets++
+		case c == ']' && quotes%2 == 0:
+			brackets--
+		case c == '\'' && i > 0 && tag[i-1] != '\\':
+			quotes++
+		case c == ',' && brackets == 0 && quotes%2 == 0:
+			tn, tv, tp := tag[tokenStart:i], "", ""
+			if p := strings.IndexByte(tn, '='); p != -1 {
+				tn, tv = tn[:p], tn[p+1:]
+			}
+			ci, ok := TagOptions[tn]
+			if !ok {
+				return nil, fmt.Errorf(`gowl/validator: unknown tag option "%s"`, tn)
+			}
+			if tv != "" && tv[0] != '[' && tv[0] != '\'' {
+				// extract value prefix ("u8:", "f32:", etc)
+				if p := strings.IndexByte(tv, ':'); p != -1 {
+					tv, tp = tv[p+1:], tv[:p]
+				}
+			}
+			var constraint ConstraintInterface
+			if constraint, err = ci(TagOption{tn, tv, tp}); err != nil {
+				return nil, err
+			}
+			constraints = append(constraints, constraint)
+			tokenStart = i + 1
 		}
-		constraint, err := ci(TagOption{match[1], match[2]})
-		if err != nil {
-			return nil, err
-		}
-		constraints = append(constraints, constraint)
 	}
 	return
 }
 
 var TagOptions = map[string]func(o TagOption) (constraint ConstraintInterface, err error){
 	"required": func(o TagOption) (constraint ConstraintInterface, err error) {
-		return o.WithBool(func(v bool) ConstraintInterface { return Required(v) }, true)
+		return o.WithBool(func(v bool) ConstraintInterface { return Required(v) })
 	},
 	"valid": func(o TagOption) (constraint ConstraintInterface, err error) {
-		return o.WithBool(func(v bool) ConstraintInterface { return Valid(v) }, true)
+		return o.WithBool(func(v bool) ConstraintInterface { return Valid(v) })
+	},
+	"type": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.WithString(func(v string) ConstraintInterface { return Type(v) })
 	},
 	"len": func(o TagOption) (constraint ConstraintInterface, err error) {
-		return o.WithUint(func(v uint) ConstraintInterface { return Length(v) })
+		return o.WithUint(func(v uint64) ConstraintInterface { return Length(v) })
 	},
 	"minlen": func(o TagOption) (constraint ConstraintInterface, err error) {
-		return o.WithUint(func(v uint) ConstraintInterface { return MinLength(v) })
+		return o.WithUint(func(v uint64) ConstraintInterface { return MinLength(v) })
 	},
 	"maxlen": func(o TagOption) (constraint ConstraintInterface, err error) {
-		return o.WithUint(func(v uint) ConstraintInterface { return MaxLength(v) })
+		return o.WithUint(func(v uint64) ConstraintInterface { return MaxLength(v) })
+	},
+	"eq": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.WithValue(func(v interface{}) ConstraintInterface { return Equal(v) })
+	},
+	"true": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.NoValue(func() ConstraintInterface { return True })
+	},
+	"false": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.NoValue(func() ConstraintInterface { return False })
+	},
+	"lt": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.WithValue(func(v interface{}) ConstraintInterface { return LessThan(v) })
+	},
+	"lte": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.WithValue(func(v interface{}) ConstraintInterface { return LessThanOrEqual(v) })
+	},
+	"gt": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.WithValue(func(v interface{}) ConstraintInterface { return GreaterThan(v) })
+	},
+	"gte": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.WithValue(func(v interface{}) ConstraintInterface { return GreaterThanOrEqual(v) })
+	},
+	"re": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.WithString(func(v string) ConstraintInterface { return Regexp(v) })
+	},
+	"repsx": func(o TagOption) (constraint ConstraintInterface, err error) {
+		return o.WithString(func(v string) ConstraintInterface { return RegexpPOSIX(v) })
 	},
 }
 
